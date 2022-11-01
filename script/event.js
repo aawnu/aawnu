ed12a4870e84e816a8d90a5add61ecb79fdb7e3f
Alex Westergaard <aw.freelancer@gmail.com>
2022-11-01 22:07:02 +0100

    Add hover-flashlight cards

---
+++ script/event.js
@@ -0,0 +1,10 @@
+document.querySelector(".hero main").onmousemove = e => {
+    for (const card of document.querySelectorAll(".hero main .blob")) {
+        const rect = card.getBoundingClientRect()
+        const x = e.clientX - rect.left
+        const y = e.clientY - rect.top
+
+        card.style.setProperty("--mouse-x", `${x}px`)
+        card.style.setProperty("--mouse-y", `${y}px`)
+    }
+}
